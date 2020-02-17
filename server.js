2b39400fc5a1703a0f369fe3d3984e026d241d14
debauchery1st <trevor.j.martin@gmail.com>
2020-02-17 16:48:26 -0500

    put on a helmet

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
-
+const helmet = require("helmet");
 const server = express();
 
 const accountsRouter = require("./routers/accounts");
 
 server.use(express.json());
+server.use(helmet());
 
 server.get("/", (req, res) => {
   res.status(200).send("hi");
