fc2210953d7381f0aa670f94d8a4c8ae8ea27733
debauchery1st <trevor.j.martin@gmail.com>
2020-02-17 15:49:00 -0500

    queries.sql, started building router

---
+++ routers/accounts.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const db = require("../data/dbConfig.js");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  db("accounts")
+    .then(accounts => {
+      res.status(200).json(accounts);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: "failed to get the list of accounts" });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  //
+});
+
+module.exports = router;
