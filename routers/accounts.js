9424482f379166f5ca546246f01e7d440b519f50
debauchery1st <trevor.j.martin@gmail.com>
2020-02-17 16:10:13 -0500

    GET by id

--- routers/accounts.js
+++ routers/accounts.js
@@ -15,7 +15,10 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-  //
+  db("accounts")
+    .where({ id: req.params.id })
+    .then(account => res.status(200).json(account[0]))
+    .catch(errors => res.status(400).json({ error: "failed to get account" }));
 });
 
 module.exports = router;
